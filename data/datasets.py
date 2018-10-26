ffa1030f1e753f4d62104d8f7c1c176c4ceb0888
LucasX <xulu0620@gmail.com>
2018-10-26 23:49:12 +0800

    update label

--- data/datasets.py
+++ data/datasets.py
@@ -14,9 +14,9 @@ from config.cfg import cfg
 
 class PlantsDataset(Dataset):
     """
-    Xingse Plants dataset
+    XingSe Plants dataset
     https://github.com/visipedia/fgvcx_flower_comp
-    Note: testset has no annotations
+    Note: testset has no annotations, hence we just use training set to manually form our train/val/test dataset
     """
 
     def __init__(self, type=True, transform=None):
@@ -32,21 +32,28 @@ class PlantsDataset(Dataset):
                 os.path.join(cfg['config']['FGVC']['root'], 'FGVC', 'train', train_cfg['images'][i]['file_name'][1:]))
             labels.append(train_cfg['annotations'][i]['category_id'])
 
-        train_images, val_images, train_labels, val_labels = train_test_split(images, labels, test_size=0.3,
-                                                                              random_state=42)
+        train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2,
+                                                                                random_state=42, stratify=labels)
+
+        train_images, val_images, train_labels, val_labels = train_test_split(train_images, train_labels, test_size=0.2,
+                                                                              random_state=2, stratify=train_labels)
 
         if type == 'train':
             images = train_images
             labels = train_labels
-        elif type == 'validation':
+        elif type == 'val':
             images = val_images
             labels = val_labels
+        elif type == 'test':
+            images = test_images
+            labels = test_labels
         else:
-            with open(test_json, mode='rt', encoding='utf-8') as f:
-                test_cfg = json.load(f)
-            for i in range(len(test_cfg['images'])):
-                images.append(
-                    os.path.join(cfg['config']['FGVC']['root'], 'FGVC', 'test', test_cfg['images'][i]['file_name']))
+            print('Invalid data type!! It supports train/val/test only!!')
+            # with open(test_json, mode='rt', encoding='utf-8') as f:
+            #     test_cfg = json.load(f)
+            # for i in range(len(test_cfg['images'])):
+            #     images.append(
+            #         os.path.join(cfg['config']['FGVC']['root'], 'FGVC', 'test', test_cfg['images'][i]['file_name']))
 
         self.images = images
         self.labels = labels
@@ -57,7 +64,7 @@ class PlantsDataset(Dataset):
 
     def __getitem__(self, idx):
         image = io.imread(self.images[idx])
-        sample = {'image': image, "label": self.labels[idx]}
+        sample = {'image': image, "label": self.labels[idx], 'filename': self.images[idx]}
 
         if self.transform:
             sample['image'] = self.transform(Image.fromarray(sample['image'].astype(np.uint8)))
--- main/run_resnet.py
+++ main/run_resnet.py
@@ -1,5 +1,7 @@
+import copy
 import os
 import sys
+import time
 
 import torch
 import torch.nn as nn
@@ -11,6 +13,8 @@ sys.path.append('../')
 from util.file_utils import mkdirs_if_not_exist
 from config.cfg import cfg
 from data import data_loader
+
+dataset_sizes = {x: len(data_loader[x]) for x in ['train', 'val', 'test']}
 
 
 def train_model(model, train_dataloader, test_dataloader, criterion, optimizer, scheduler, num_epochs,
@@ -95,11 +99,119 @@ def train_model(model, train_dataloader, test_dataloader, criterion, optimizer, 
     print('Accuracy of ResNet: %f' % (correct / total))
 
 
-def run_resnet(train_dataloader, test_dataloader):
+def train_model_ft(model, dataloaders, criterion, optimizer, scheduler, num_epochs, inference):
+    """
+    train model with fine-tune on ImageNet
+    :param dataloaders:
+    :param model:
+    :param criterion:
+    :param optimizer:
+    :param scheduler:
+    :param num_epochs:
+    :param inference:
+    :return:
+    """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
+    model = model.to(device)
+
+    if not inference:
+        since = time.time()
+
+        best_model_wts = copy.deepcopy(model.state_dict())
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+            print('-' * 100)
+
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    scheduler.step()
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()  # Set model to evaluate mode
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+
+                epoch_loss = running_loss / (dataset_sizes[phase] * cfg['batch_size'])
+                epoch_acc = running_corrects.double() / (dataset_sizes[phase] * cfg['batch_size'])
+
+                print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                    phase, epoch_loss, epoch_acc))
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    best_model_wts = copy.deepcopy(model.state_dict())
+
+                    model_path_dir = './model'
+                    mkdirs_if_not_exist(model_path_dir)
+                    torch.save(model.state_dict(), os.path.join(model_path_dir, '{0}_Epoch_{1}.pth'.format(
+                        model.__class__.__name__, epoch)))
+
+        time_elapsed = time.time() - since
+        print('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        print('Best val Acc: {:4f}'.format(best_acc))
+
+        # load best model weights
+        model.load_state_dict(best_model_wts)
+
+    else:
+        print('Loading pre-trained model...')
+        model.load_state_dict(torch.load(os.path.join('./model/%s.pth' % model.__class__.__name__)))
+
+    model.eval()
+
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in dataloaders['test']:
+            images, label = data['image'], data['label']
+            images = images.to(device)
+            label = label.to(device)
+
+            pred = model.forward(images)
+            _, predicted = torch.max(pred.data, 1)
+            total += pred.size(0)
+            correct += (predicted == label).sum().item()
+
+    print('Accuracy of ResNet: %f' % (correct / total))
+
+
+def run_resnet(epoch, inference=False):
     """
     train and eval on ResNet
-    :param train_dataloader:
-    :param test_dataloader:
     :return:
     """
     resnet = models.resnet50(pretrained=True)
@@ -112,11 +224,14 @@ def run_resnet(train_dataloader, test_dataloader):
 
     exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=40, gamma=0.1)
 
-    train_model(model=resnet, train_dataloader=train_dataloader, test_dataloader=test_dataloader,
-                criterion=criterion, optimizer=optimizer, scheduler=exp_lr_scheduler, num_epochs=cfg['config']['FGVC']['epoch'],
-                inference=False)
+    train_model_ft(model=resnet, dataloaders=data_loader.load_data("FGVC"), criterion=criterion,
+                   optimizer=optimizer, scheduler=exp_lr_scheduler, num_epochs=epoch, inference=inference)
+
+    # train_model(model=resnet, train_dataloader=train_dataloader, test_dataloader=test_dataloader,
+    #             criterion=criterion, optimizer=optimizer, scheduler=exp_lr_scheduler,
+    #             num_epochs=cfg['config']['FGVC']['epoch'],
+    #             inference=False)
 
 
 if __name__ == '__main__':
-    trainloader, testloader = data_loader.load_data("FGVC")
-    run_resnet(trainloader, testloader)
+    run_resnet(epoch=200, inference=False)
