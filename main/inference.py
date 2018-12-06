12686d7cfc44765d8d0f3dda9236a5eb57c21dda
LucasX <xulu0620@gmail.com>
2018-12-06 18:15:48 +0800

    update label

--- main/inference.py
+++ main/inference.py
@@ -1,9 +1,11 @@
 # Model Inference
 import time
+import base64
 from pprint import pprint
 
 import numpy as np
 import pandas as pd
+import requests
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -40,7 +42,8 @@ class PlantRecognizer():
         #     for k, v in state_dict.items():
         #         name = k[7:]  # remove `module.`
         #         new_state_dict[name] = v
-        #         model.load_state_dict(new_state_dict)
+        #
+        #     model.load_state_dict(new_state_dict)
 
         model.to(device)
         model.eval()
@@ -96,10 +99,68 @@ class PlantRecognizer():
             ]
         }
 
+    def infer_from_img_url(self, img_url):
+        tik = time.time()
+        response = requests.get(img_url, timeout=20)
+        if response.status_code in [403, 404, 500]:
+            return {
+                'status': 2,
+                'message': 'Invalid URL',
+                'elapse': time.time() - tik,
+                'results': None
+            }
+
+        else:
+            img_content = response.content
+
+            import io
+            from PIL import Image
+            img_np = np.array(Image.open(io.BytesIO(img_content)))
+            img = Image.fromarray(img_np.astype(np.uint8))
+
+            preprocess = transforms.Compose([
+                transforms.Resize(227),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])
+
+            img = preprocess(img)
+            img.unsqueeze_(0)
+
+            img = img.to(self.device)
+
+            outputs = self.model.forward(img)
+            outputs = F.softmax(outputs, dim=1)
+
+            # get TOP-K output labels and corresponding probabilities
+            topK_prob, topK_label = torch.topk(outputs, self.topK)
+            prob = topK_prob.to("cpu").detach().numpy().tolist()
+
+            _, predicted = torch.max(outputs.data, 1)
+
+            tok = time.time()
+
+            return {
+                'status': 0,
+                'message': 'success',
+                'elapse': tok - tik,
+                'results': [
+                    {
+                        'name': self.key_type[int(topK_label[0][i].to("cpu"))],
+                        'category id': int(topK_label[0][i].data.to("cpu").numpy()) + 1,
+                        'prob': round(prob[0][i], 4)
+                    } for i in range(self.topK)
+                ]
+            }
+
 
 if __name__ == '__main__':
     plant_recognizer = PlantRecognizer('./model/ResNet50_Plant.pth')
+    # pprint(plant_recognizer.infer('./{0}.jpg'.format(5)))
+    pprint(plant_recognizer.infer_from_img_url(
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1544099445996&di=2716838202341cfddf9d3c5d58a76eaf&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201502%2F28%2F20150228103438_NQG3u.thumb.700_0.jpeg"))
 
-    for i in range(4):
-        pprint(plant_recognizer.infer('./{0}.jpg'.format(i + 1)))
-        print('=' * 100)
+    # for i in range(4):
+    #     pprint(plant_recognizer.infer('./{0}.jpg'.format(i + 1)))
+    #     print('=' * 100)
